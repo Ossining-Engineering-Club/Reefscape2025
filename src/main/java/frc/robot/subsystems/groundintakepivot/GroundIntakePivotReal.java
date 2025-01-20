98ec7e6e3aa524156b4381b54bcc6d8cbe7cddd1
itserichuang <141202406+itserichuang@users.noreply.github.com>
2025-01-19 21:28:54 -0500

    finished ground intake pivot subsystem

--- src/main/java/frc/robot/subsystems/groundintakepivot/GroundIntakePivotReal.java
+++ src/main/java/frc/robot/subsystems/groundintakepivot/GroundIntakePivotReal.java
@@ -2,30 +2,38 @@ package frc.robot.subsystems.groundintakepivot;
 
 import static frc.robot.subsystems.groundintakepivot.GroundIntakePivotConstants.*;
 
-import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
 
 public class GroundIntakePivotReal implements GroundIntakePivotIO {
-    private final SparkMax sparkMax;
+  private final SparkMax sparkMax;
+  private final RelativeEncoder encoder;
 
-    public GroundIntakePivotReal() {
-        sparkMax = new SparkMax(canId, MotorType.kBrushless);
+  public GroundIntakePivotReal() {
+    sparkMax = new SparkMax(canId, MotorType.kBrushless);
+    encoder = sparkMax.getEncoder();
 
-        var config = new SparkMaxConfig();
-        config
-            .inverted(isInverted)
-            .idleMode(IdleMode.kBrake);
-        config
-            .encoder
-            .positionConversionFactor(1.0/motorReduction * encoderPositionFactor)
-            .velocityConversionFactor(1.0/motorReduction * encoderVelocityFactor);
-    }
+    var config = new SparkMaxConfig();
+    config.inverted(isInverted).idleMode(IdleMode.kBrake);
+    config
+        .encoder
+        .positionConversionFactor(1.0 / motorReduction * encoderPositionFactor)
+        .velocityConversionFactor(1.0 / motorReduction * encoderVelocityFactor);
+    
+    encoder.setPosition(startAngle);
+  }
 
-    @Override
-    public void updateInputs(GroundIntakePivotIOInputs inputs) {
-        inputs.appliedVolts = sparkMax.getAppliedOutput() * sparkMax.getBusVoltage();
+  @Override
+  public void updateInputs(GroundIntakePivotIOInputs inputs) {
+    inputs.appliedVolts = sparkMax.getAppliedOutput() * sparkMax.getBusVoltage();
+    inputs.angleRadians = encoder.getPosition();
+  }
 
-    }
+  @Override
+  public void setVoltage(double voltage) {
+    sparkMax.setVoltage(voltage);
+  }
 }
