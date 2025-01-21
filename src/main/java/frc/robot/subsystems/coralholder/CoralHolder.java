d575c82ce00e69d3aed6db234ecff1ddd26b2f8e
Steve Suscal <steve12s@icloud.com>
2025-01-20 23:45:35 -0500

    Almost Finished Implementation Coral Holder
    Added almost all IO.

--- src/main/java/frc/robot/subsystems/coralholder/CoralHolder.java
+++ src/main/java/frc/robot/subsystems/coralholder/CoralHolder.java
@@ -1,3 +1,46 @@
 package frc.robot.subsystems.coralholder;
 
-public class CoralHolder {}
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.spark.SparkMax;
+import frc.robot.subsystems.coralholder.CoralHolderConstants.*;
+
+import static frc.robot.subsystems.coralholder.CoralHolderConstants.k_beamId;
+import static frc.robot.subsystems.coralholder.CoralHolderConstants.k_powerPercent;
+
+import org.littletonrobotics.junction.Logger;
+import frc.robotics.subsytems.breakbeam;
+
+public class CoralHolder extends SubsystemBase {
+   private final CoralHolderIO io;
+
+   private final breakbeam holderBeam;
+
+   private final CoralHolderIOInputsAutoLogged inputs = new 
+
+    public CoralHolder() {
+        this.io = io;
+        holderBeam = new BreakBeam(io, k_beamId);
+    }
+
+
+    @Override
+    public void periodic() {
+        io.updateInputs(inputs);
+
+        Logger.processInputs("Coral Holder", inputs);
+
+        if (holderBeam.isTripped()) turnOff();
+    }
+
+    public double turnOff() {
+        io.setVoltage(0);
+    }
+
+    public double turnOn() {
+        io.setVoltage(1*k_powerPercent);
+    }
+
+    public void breakBeamCheck() {
+        return holderBeam.isTripped();
+    }
+}
