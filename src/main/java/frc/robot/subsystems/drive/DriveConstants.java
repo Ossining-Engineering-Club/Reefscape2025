08560c89b3b9c1c88fd5a7d6de08126c7a7ecdf5
itserichuang <141202406+itserichuang@users.noreply.github.com>
2025-01-24 10:54:37 -0500

    updated robot config for pathplanner

--- src/main/java/frc/robot/subsystems/drive/DriveConstants.java
+++ src/main/java/frc/robot/subsystems/drive/DriveConstants.java
@@ -99,9 +99,9 @@ public class DriveConstants {
   public static final double turnPIDMaxInput = 2 * Math.PI; // Radians
 
   // PathPlanner configuration
-  public static final double robotMassKg = 74.088;
-  public static final double robotMOI = 6.883;
-  public static final double wheelCOF = 1.2;
+  public static final double robotMassKg = 52.806;
+  public static final double robotMOI = 4.14;
+  public static final double wheelCOF = 1.0;
   public static final RobotConfig ppConfig =
       new RobotConfig(
           robotMassKg,
