0b7e796878d62590c5f98d7a4c36a107b702635b
Kyle7004 <128335496+Kyle7004@users.noreply.github.com>
2025-01-26 20:26:13 -0500

    Added AlgaeClaw subsystem

--- src/main/java/frc/robot/subsystems/algaeclaw/AlgaeClaw.java
+++ src/main/java/frc/robot/subsystems/algaeclaw/AlgaeClaw.java
@@ -1,3 +1,25 @@
 package frc.robot.subsystems.algaeclaw;
 
-public class AlgaeClaw {}
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class AlgaeClaw extends SubsystemBase {
+
+    private final SparkMax clawMotor;
+
+    /** Algae Claw construction */
+    public AlgaeClaw() {
+      clawMotor = new SparkMax(AlgaeClawConstants.clawCANID, MotorType.kBrushless);
+    }
+  
+    /** Sets motor voltage to predefined voltage */
+    public void startMotorForwards() {
+      clawMotor.setVoltage(AlgaeClawConstants.clawVoltage);
+    }
+  
+    /** Sets motor voltage to zero */
+    public void stopMotor() {
+      clawMotor.setVoltage(0.0);
+    }
+}
