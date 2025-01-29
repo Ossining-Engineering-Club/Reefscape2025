25e458970aa6bb6da7d175c4b2be6ff7d45e4606
Kyle7004 <kbattacharia0606@students.ossiningufsd.org>
2025-01-29 00:58:05 -0500

    Added AlgaeClaw/GroundIntake forward intaking commands using breakbeams

--- src/main/java/frc/robot/subsystems/algaeclaw/AlgaeClaw.java
+++ src/main/java/frc/robot/subsystems/algaeclaw/AlgaeClaw.java
@@ -3,14 +3,17 @@ package frc.robot.subsystems.algaeclaw;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.breakbeam.Breakbeam;
 
 public class AlgaeClaw extends SubsystemBase {
 
   private final SparkMax clawMotor;
+  private final Breakbeam breakbeam;
 
   /** Algae Claw construction */
-  public AlgaeClaw() {
+  public AlgaeClaw(Breakbeam breakbeam) {
     clawMotor = new SparkMax(AlgaeClawConstants.clawCANID, MotorType.kBrushless);
+    this.breakbeam = breakbeam;
   }
 
   /** Sets motor voltage to predefined voltage */
@@ -26,5 +29,10 @@ public class AlgaeClaw extends SubsystemBase {
   /** Sets motor voltage to zero */
   public void stopMotor() {
     clawMotor.setVoltage(0.0);
+  }
+
+  /** Gets state of AlgaeClaw breakbeam */
+  public boolean isBBTripped() {
+    return breakbeam.isTripped();
   }
 }
