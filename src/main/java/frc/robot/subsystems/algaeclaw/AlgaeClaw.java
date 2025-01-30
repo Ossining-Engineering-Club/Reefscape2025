19b920fd505e1310d12a1e4418f513688fd705a9
itserichuang <141202406+itserichuang@users.noreply.github.com>
2025-01-30 09:26:37 -0500

    added io layer to algae claw

--- src/main/java/frc/robot/subsystems/algaeclaw/AlgaeClaw.java
+++ src/main/java/frc/robot/subsystems/algaeclaw/AlgaeClaw.java
@@ -1,38 +1,37 @@
 package frc.robot.subsystems.algaeclaw;
 
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.SparkMax;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.algaeclaw.AlgaeClawIO.AlgaeClawIOInputs;
 import frc.robot.subsystems.breakbeam.Breakbeam;
 
 public class AlgaeClaw extends SubsystemBase {
-
-  private final SparkMax clawMotor;
   private final Breakbeam breakbeam;
+  private final AlgaeClawIO io;
+  private final AlgaeClawIOInputs inputs = new AlgaeClawIOInputs();
 
   /** Algae Claw construction */
-  public AlgaeClaw(Breakbeam breakbeam) {
-    clawMotor = new SparkMax(AlgaeClawConstants.clawCANID, MotorType.kBrushless);
+  public AlgaeClaw(AlgaeClawIO io, Breakbeam breakbeam) {
+    this.io = io;
     this.breakbeam = breakbeam;
   }
 
   /** Sets motor voltage to predefined voltage */
   public void startMotor() {
-    clawMotor.setVoltage(AlgaeClawConstants.clawVoltage);
+    io.setVoltage(AlgaeClawConstants.clawVoltage);
   }
 
   /** Reverses algae claw motor */
   public void reverseMotor() {
-    clawMotor.setVoltage(AlgaeClawConstants.reverseVoltage);
+    io.setVoltage(AlgaeClawConstants.reverseVoltage);
   }
 
   /** Sets motor voltage to zero */
   public void stopMotor() {
-    clawMotor.setVoltage(0.0);
+    io.setVoltage(0.0);
   }
 
   /** Gets state of AlgaeClaw breakbeam */
-  public boolean isBBTripped() {
+  public boolean hasAlgae() {
     return breakbeam.isTripped();
   }
 }
