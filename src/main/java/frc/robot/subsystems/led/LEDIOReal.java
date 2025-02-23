e5c687a4fc5b8aac567617883ab253c6ddbba902
Steve Suscal <steve12s@icloud.com>
2025-02-23 12:24:18 -0500

    Finished robot leds, now photoelectric sensor implementation

---
+++ src/main/java/frc/robot/subsystems/led/LEDIOReal.java
@@ -0,0 +1,29 @@
+package frc.robot.subsystems.led;
+
+import static frc.robot.subsystems.led.LEDConstants.*;
+
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+
+public class LEDIOReal implements LEDIO {
+    private final AddressableLED leds;
+
+    private AddressableLEDBuffer buffer;
+
+
+    public LEDIOReal() {
+        leds = new AddressableLED(pwmPort);
+        buffer = new AddressableLEDBuffer(120);
+    }
+
+    @Override
+    public void setData(AddressableLEDBuffer buffer) {
+        leds.setData(buffer);
+    }
+
+    
+
+    
+}
