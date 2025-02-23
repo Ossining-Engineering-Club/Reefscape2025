ae2fe8d55dadc9d027fab7b73db4742bf97b4d00
itserichuang <141202406+itserichuang@users.noreply.github.com>
2025-02-22 21:35:30 -0500

    added free spinning current limit for elevator

--- src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
@@ -10,7 +10,8 @@ public class ElevatorConstants {
     public static final double encoderPositionFactor = 2 * Math.PI; // Rotations -> Radians
     public static final double encoderVelocityFactor = (2 * Math.PI) / 60.0; // RPM -> Rad/Sec
     public static final boolean isInverted = false;
-    public static final int currentLimit = 30;
+    public static final int stallCurrentLimit = 30;
+    public static final int freeCurrentLimit = 50;
 
     // control system
     public static final double kP = 1.0;
