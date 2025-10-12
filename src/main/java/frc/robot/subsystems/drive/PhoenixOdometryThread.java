56233069fded5ea2fe04a847e1f9ab87f4f1198b
itserichuang <141202406+itserichuang@users.noreply.github.com>
2025-10-12 12:02:15 -0400

    changed PhoenixOdometryThread to always have canfd off

--- src/main/java/frc/robot/subsystems/drive/PhoenixOdometryThread.java
+++ src/main/java/frc/robot/subsystems/drive/PhoenixOdometryThread.java
@@ -14,11 +14,9 @@
 package frc.robot.subsystems.drive;
 
 import com.ctre.phoenix6.BaseStatusSignal;
-import com.ctre.phoenix6.CANBus;
 import com.ctre.phoenix6.StatusSignal;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj.RobotController;
-import frc.robot.generated.TunerConstants;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Queue;
@@ -44,8 +42,8 @@ public class PhoenixOdometryThread extends Thread {
     private final List<Queue<Double>> genericQueues = new ArrayList<>();
     private final List<Queue<Double>> timestampQueues = new ArrayList<>();
 
-    private static boolean isCANFD =
-            new CANBus(TunerConstants.DrivetrainConstants.CANBusName).isNetworkFD();
+    private static boolean isCANFD = false;
+    // new CANBus(TunerConstants.DrivetrainConstants.CANBusName).isNetworkFD();
     private static PhoenixOdometryThread instance = null;
 
     public static PhoenixOdometryThread getInstance() {
